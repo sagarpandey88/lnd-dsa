026bf39ba767a94c17330485d86b5e93db164011
sagarpandey88 <30863296+sagarpandey88@users.noreply.github.com>
2023-09-28 21:35:59 +0530

    added sorting methods

---
+++ algorithms/sorting/sorting.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * -----------Basic sorting algo-----------
+ * bubble sort : swapping the numbers by comparing left to right and loop through the length of array
+ * insertion sort :
+ * selection sort
+ *
+ * --------------------
+ *  Merge sort
+ *  quick sort
+ *
+ */
