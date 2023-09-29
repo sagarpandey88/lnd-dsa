88de7ffb4bb07ca5dc8a967d3022459a4a9c5d66
sagarpandey88 <30863296+sagarpandey88@users.noreply.github.com>
2023-09-29 21:09:27 +0530

    selection sort self made

--- algorithms/sorting/selectionsort.js
+++ algorithms/sorting/selectionsort.js
@@ -11,7 +11,7 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
   for (let i = 0; i < array.length; i++) {
-    let min = i; // assume current index is minimum
+    let min = i; // assume outer loop current index is minimum
     let temp = array[i]; // set the value of current index in a temp variable
 
     //looping through current index +1 items
@@ -29,5 +29,25 @@ function selectionSort(array) {
 }
 
 export function init() {
-  console.log(selectionSort(numbers));
+  console.log(selectionSortV2(numbers));
+}
+
+//self made.
+function selectionSortV2(array) {
+  for (let i = 0; i < array.length; i++) {
+    let min = i; //assuming the current index
+    let temp = array[min]; // saving the value of current index so that it can be swapped when another min is found
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        // if the current index is less than the min store the min index
+        min = j;
+      }
+    }
+
+    // once we have looped through all the items swap with the new min
+    array[i] = array[min]; //set the value of i with the min value found
+    array[min] = temp; // set the min index to the value found.
+  }
+  return array;
 }
